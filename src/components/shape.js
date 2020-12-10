4cdb416cd62d18975e85f8977fcb5eddded036ea
shariquearshad <shariquearshad@gmail.com>
2020-12-11 00:17:36 +0530

    Create shape.js

---
+++ src/components/shape.js
@@ -0,0 +1,15 @@
+class Shape {
+  constructor() {
+    this.color = "blue";
+  }
+
+  drawShape() {
+    console.log("draw shape");
+  }
+
+  calculateArea() {
+    console.log("shape calc area");
+  }
+}
+
+module.exports = Shape;
